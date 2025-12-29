bb2e648c752ab65ed1a6dfd4c72efb174cc7b61c
Eirele Joven Boter <38448465+eirele01@users.noreply.github.com>
2025-12-30 02:09:40 +0800

    Apply latest updates and fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'KaraOkay',
+      title: 'KaraOkey',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' }
